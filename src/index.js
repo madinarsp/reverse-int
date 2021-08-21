db40b1e7ef5ecde8cb8e3855204b4e133c465520
madinarsp <madinarsp20@gmail.com>
2021-08-21 15:43:51 +0600

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let rem, str = '', tempNum;
+    tempNum = n < 0 ? Math.abs(n) : n;
+    while(tempNum) {
+        rem = tempNum % 10;
+        str += rem;
+        tempNum = Math.trunc(tempNum / 10);
+    }
+    return +str;
 }
